2d9c7dc70ee03f42babf6da6a8d41aef0836fce7
Cj-Scott <30878277+Cj-Scott@users.noreply.github.com>
2018-01-11 14:58:44 -0500

    Created Shutdown-VMs_Priority2.ps1 in AzureAutomation.

---
+++ MAG-DOS/Shutdown-VMs_Priority2.ps1
@@ -0,0 +1,48 @@
+<#
+    .DESCRIPTION
+        A runbook which Stops VM's that are tagged with key of Priority and Value of 2 using the Run As Account (Service Principal)
+
+    .NOTES
+        AUTHOR: CHSC
+        LASTEDIT: October 26, 2016
+#>
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Login-AzureRmAccount -environment AzureUSGovernment `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+# Get all VMs in the subscription and Shut them down if they are running
+[array]$VMs = Get-AzureRMVm -Status | `
+Where-Object {$PSItem.Tags.Keys -eq "Priority" -and $PSItem.Tags.Values -eq "2" `
+-and $PSItem.PowerState -eq "VM running"}
+ 
+ForEach ($VM in $VMs) 
+{
+    Write-Output "Shutting down: $($VM.Name)"
+    Stop-AzureRMVM -Name $VM.Name -ResourceGroupName $VM.ResourceGroupName -Force
+}     
+
+
+Start-AzureRmAutomationRunbook -Name Shutdown-VMs_Priority1 -AutomationAccountName OPSMG01-OMS-Automation -ResourceGroupName opsmg01-oms
+
