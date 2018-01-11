4c86bcb8f786432889ff6e8dbeb04043d478b4b4
Cj-Scott <30878277+Cj-Scott@users.noreply.github.com>
2018-01-11 14:55:05 -0500

    Created LoginAzureRM_Gov.ps1 in AzureAutomation.

---
+++ MAG-DOS/LoginAzureRM_Gov.ps1
@@ -0,0 +1,26 @@
+
+#If you used a custom RunAsConnection during the Automation Account setup this will need to reflect that.
+$connectionName = "AzureRunAsConnection" 
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Login-AzureRmAccount -environment AzureUSGovernment `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
